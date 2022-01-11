a3159fca131eefe6b6c4c4eab7015c7ecb40ee86
Austin Carlson <austincarl24@gmail.com>
2022-01-10 20:10:01 -0600

    Messed around with If/Else statments and continued my C# practice.

---
+++ SelectionStatments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("I'm thinking of a number between 1 and 1000. Can you guess it? Type your guess below (NO LETTERS!!!) good luck :)");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            while (userInput != favNumber)
+            {
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("OOOOP Litttle too high!");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("OOpsie little too low there!");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                
+            }
+            Console.WriteLine("You got 'er! Congrats!");
+        }
+    }
+}
